d8c5211a4073c0600c21926d25e3f6bcb499ba3c
trinteen <140308014+trinteen@users.noreply.github.com>
2025-03-24 22:20:34 +0100

    Update keymap.sh

--- modules/keymap.sh
+++ modules/keymap.sh
@@ -7,7 +7,10 @@ install_gui_keymap_func() {
     export KEYMAP_NAME=$(localectl list-keymaps | less > cache_keymap)
     export KEYMAP_MENU=()
     while IFS= read -r line; do
-        LANGUAGE_MENU+=("${line} ${line}")
+        if [[ ${#line} == 2 ]] || \
+           [[ ${line} == *"-qwert"* ]];then
+            LANGUAGE_MENU+=("${line} ${line}")
+        fi
     done < cache_keymap
     export GUI_KEYMAP=$(dialog --title "Keymap" --backtitle "${AUTHOR_T}" --stdout --menu "Select :" ${WINDOW_H} ${WINDOW_W} ${WINDOW_L} ${LANGUAGE_MENU[@]})
     install_gui_keymap=${GUI_KEYMAP}
